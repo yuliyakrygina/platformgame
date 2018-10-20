87e62b672359fa5de99c181995010e7e5e69960e
Yuliya Krygina <36555259+yuliyakrygina@users.noreply.github.com>
2018-10-19 22:14:26 -0400

    Level made/ Player movement

---
+++ platformgame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float MoveSpeed; //we see it in Unity 
+	private Rigidbody2D MyRigidBody;
+
+	void Start () {
+		MyRigidBody = GetComponent<Rigidbody2D>(); //get component from inspector, attach rigidbody, will know its player
+	}
+	
+	void Update () {
+		
+		if(Input.GetAxisRaw ("Horizontal") > 0f) //horizontal movement, calling input 
+		{
+			MyRigidBody.velocity = new Vector3(MoveSpeed, MyRigidBody.velocity.y, 0f); // 0f, zero float
+		}
+		else if(Input.GetAxisRaw("Horizontal") < 0f) //horizontal movement, calling input 
+		{
+			MyRigidBody.velocity = new Vector3(-MoveSpeed, MyRigidBody.velocity.y, 0f); // 0f, zero float
+		}
+		else
+		{
+			MyRigidBody.velocity = new Vector3(0f, MyRigidBody.velocity.y, 0f);
+		}
+
+	}
+}
