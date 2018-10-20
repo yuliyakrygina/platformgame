43839dede27264b7fa84cd12ad8aae5b12946bd9
Yuliya Krygina <36555259+yuliyakrygina@users.noreply.github.com>
2018-10-19 23:00:46 -0400

    Player Controlls

--- platformgame/Assets/Scripts/PlayerController.cs
+++ platformgame/Assets/Scripts/PlayerController.cs
@@ -7,12 +7,23 @@ public class PlayerController : MonoBehaviour {
 	public float MoveSpeed; //we see it in Unity 
 	private Rigidbody2D MyRigidBody;
 
+	public float JumpSpeed;
+
+	public Transform GroundCheck;
+	public float GroundCheckRadius;
+	public LayerMask WhatIsGround; //what are we using
+
+	public bool IsGrounded; //true or false statement
+
 	void Start () {
 		MyRigidBody = GetComponent<Rigidbody2D>(); //get component from inspector, attach rigidbody, will know its player
 	}
 	
 	void Update () {
-		
+
+		IsGrounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, WhatIsGround);
+
+
 		if(Input.GetAxisRaw ("Horizontal") > 0f) //horizontal movement, calling input 
 		{
 			MyRigidBody.velocity = new Vector3(MoveSpeed, MyRigidBody.velocity.y, 0f); // 0f, zero float
@@ -25,6 +36,12 @@ public class PlayerController : MonoBehaviour {
 		{
 			MyRigidBody.velocity = new Vector3(0f, MyRigidBody.velocity.y, 0f);
 		}
+
+		if(Input.GetButtonDown("Jump") && IsGrounded)
+		{
+			MyRigidBody.velocity = new Vector3(MyRigidBody.velocity.x, JumpSpeed, 0f);
+		}
+
 
 	}
 }
